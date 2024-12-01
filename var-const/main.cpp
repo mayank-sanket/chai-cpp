31476150a50b9ecbd58e05efca1a7b9a3749d0d0
mayank-sanket <mayanksanket@gmail.com>
2024-12-02 02:28:27 +0530

    variables and constants

---
+++ var-const/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"chai with cpp"<<endl;
+    cout<<"chai with cpp \n";
+    cout<<"chai with cpp"<<"\n";
+    return 0;
+}
---
+++ var-const/var.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int score;
+    score = 110;
+
+    int lives = 3;
+    cout << score << endl;
+    cout<<(score*2)<<endl;
+    cout<<lives+1<<endl;
+
+
+    // constants:
+    
+    const int userID = 121243;
+    // userID = 989898; // cannot change the value of a const
+    cout<<userID<<endl;
+}
